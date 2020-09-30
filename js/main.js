8497a7a4612ca555b6617f21c2e975cb5db9363c
okapeeokapee <okapee420@gmail.com>
2020-10-01 01:57:27 +0900

    let loc

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 'use strict';{
+  const word = 'red';
+  let loc = 0;
+
+  const target = document.getElementById('target');
+  target.textContent = word;
+
   document.addEventListener('keydown', e =>{
-    const target = document.getElementById('target');
-    target.textContent = e.key;
+    if(e.key === word[loc]){
+      loc++;
+
+      //1:_ed
+      //2:__d
+      //3:___
+    }
   });
 }
