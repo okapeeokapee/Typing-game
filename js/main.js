61be732f5a0bd6208230c481a0a6098c536872f7
okapeeokapee <okapee420@gmail.com>
2020-10-02 15:26:04 +0900

    setword() add

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 'use strict';{
+
+  function setWord(){
+    word = words[Math.floor(Math.random() * words.length)];
+    target.textContent = word;
+    loc = 0;
+  }
+
   const words =[
     'red',
     'blue',
@@ -8,23 +15,21 @@
   
   let word;
   let loc = 0;
-
   const target = document.getElementById('target');
-  word = words[Math.floor(Math.random() * words.length)];
-  target.textContent = word;
+
+  setWord();
 
   document.addEventListener('keydown', e =>{
     if(e.key !== word[loc]){
       return;
     }
 
-    if(e.key === word[loc]){
       loc++;
 
-      //1:_ed
-      //2:__d
-      //3:___
       target.textContent ='_'.repeat(loc) + word.substring(loc);
-    }
+
+      if(loc === word.length){
+        setWord();
+      }
   });
 }
